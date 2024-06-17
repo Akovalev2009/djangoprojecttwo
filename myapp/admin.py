8f91239619c260cf9cea37bca0131e80ac82328e
Akovalev2009 <165302611+Akovalev2009@users.noreply.github.com>
2024-06-17 15:32:35 +0200

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,23 +1,6 @@
 from django.contrib import admin
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.sites.models import Site
-from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
-from django.contrib.flatpages.forms import FlatpageForm
 
-class FlatPageAdmin(FlatPageAdminOld):
-    form = FlatpageForm
-    fieldsets = (
-        (None, {'fields': ('url', 'title', 'content', 'sites')}),
-        ('Advanced options', {
-            'classes': ('collapse',),
-            'fields': (
-                'enable_comments',
-                'registration_required',
-                'template_name',
-            ),
-        }),
-    )
-
-admin.site.unregister(FlatPage)
-admin.site.register(FlatPage, FlatPageAdmin)
+admin.site.register(FlatPage)
 admin.site.register(Site)
